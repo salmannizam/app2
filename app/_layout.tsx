ebe3d28b1e7e99c7bd5b6749654668a366e31bc5
salman nizam <salmannizam.info@gmail.com>
2025-02-20 08:30:50 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // ✅ Import this
 import Footer from "@/components/Footer";
 import { Stack } from "expo-router";
 import { MD3LightTheme, PaperProvider } from "react-native-paper";
@@ -6,21 +7,24 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
-    <SafeAreaProvider>
-      <PaperProvider theme={MD3LightTheme}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <Stack
-            screenOptions={{
-              headerShown: true, // Show the header
-              headerStyle: { backgroundColor: "#1D3C6B" }, // Header background color
-              headerTintColor: "#FFFFFF", // Header text color
-              headerTitleStyle: { fontSize: 20, fontWeight: "bold" },
-            }}
-          />
-          <Footer />
-        </SafeAreaView>
-        <Toast />
-      </PaperProvider>
-    </SafeAreaProvider>
+
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <PaperProvider theme={MD3LightTheme}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Stack
+              screenOptions={{
+                headerShown: true, // Show the header
+                headerStyle: { backgroundColor: "#1D3C6B" }, // Header background color
+                headerTintColor: "#FFFFFF", // Header text color
+                headerTitleStyle: { fontSize: 20, fontWeight: "bold" },
+              }}
+            />
+            <Footer />
+          </SafeAreaView>
+          <Toast />
+        </PaperProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
